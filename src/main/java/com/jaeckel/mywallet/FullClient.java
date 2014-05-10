94bcc6108033b02c43c8d762715384cd63d5a467
Dirk Jäckel <github@dirk.jaeckel.name>
2014-05-10 18:46:51 +0200

    new FullClient

--- src/main/java/com/jaeckel/mywallet/FullClient.java
+++ src/main/java/com/jaeckel/mywallet/FullClient.java
@@ -5,7 +5,6 @@ import com.google.bitcoin.core.*;
 import com.google.bitcoin.net.discovery.PeerDiscovery;
 import com.google.bitcoin.params.MainNetParams;
 import com.google.bitcoin.store.*;
-import com.sun.tools.internal.xjc.reader.dtd.bindinfo.BIInterface;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +14,8 @@ public class FullClient {
 
     public static final Logger Log = LoggerFactory.getLogger(FullClient.class);
     NetworkParameters netParams = new MainNetParams();
+    PostgresFullPrunedBlockStore blockStore;
+
 
     public static void main(String[] args ) {
 
@@ -40,18 +41,32 @@ public class FullClient {
 
         try {
 
-            PostgresFullPrunedBlockStore blockStore = new PostgresFullPrunedBlockStore(netParams, 0, "localhost", "full_mode_db", "biafra", "");
+            blockStore = new PostgresFullPrunedBlockStore(netParams, 0, "localhost", "full_mode_db", "biafra", "");
             FullPrunedBlockChain blockChain = new FullPrunedBlockChain(netParams, blockStore);
             PeerDiscovery peerDiscovery = MyUtils.getLocalHostPeerDiscovery();
+
+            //Supposed to be faster
+            blockChain.setRunScripts(false);
+
 
             PeerGroup peerGroup = new PeerGroup(netParams, blockChain);
             peerGroup.addPeerDiscovery(peerDiscovery);
 
             peerGroup.startAndWait();
+            Log.info("Startup done. Ready for Queries.");
 
-            BigInteger balance = blockStore.calculateBalanceForAddress(new Address(netParams, "1Dh2Pzbqe15QPsrHXrurLCHpY28K6ZGQMx"));
+            String[] addresses = new String[]{
+                    "18Nnr1236PeuVXpceXKbDJbAhWK6dYxaS9",
+                    "1Dh2Pzbqe15QPsrHXrurLCHpY28K6ZGQMx",
+                    "1PBuXpUVcLZEpoTSRC1tLpyV6xtGCvEeJR",
+                    "1KBmj2QQs9yiq3wW2jEJuVnuBybWvLv6fV",
+                    "1E1xPBdS85g8Eocs28NRHU4JQ1PEdbVgPg",
+                    "1LttvufSeNniUFTMRJq46ZRbLhZoQvFVNJ"
+            };
+            for (String address : addresses) {
+                getBalaceForAdress(address);
 
-            Log.info("Balance: " + balance);
+            }
 
         } catch (BlockStoreException e) {
 
@@ -62,6 +77,13 @@ public class FullClient {
 
     }
 
+    private void getBalaceForAdress(String address) throws BlockStoreException, AddressFormatException {
+        long start = System.currentTimeMillis();
+        BigInteger balance;
+
+        balance = blockStore.calculateBalanceForAddress(new Address(netParams, address));
+        Log.info("Balance for address: " + address + " is " + balance + ". Calulated in " + (System.currentTimeMillis() - start) + "ms");
+    }
 
 
 }
